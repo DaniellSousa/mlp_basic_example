66d44b4e8f5f080479da1298c1447a98841ede88
Daniel Monteiro <daniel.monteiro@deway.com.br>
2019-06-18 13:33:54 -0300

    files private key ssh on .gitignore; and file rede_multicamada.py that already have the calculates of inputs and the multi-layer calculate and their outputs and errors

--- rede_multicamada.py
+++ rede_multicamada.py
@@ -2,7 +2,7 @@ import numpy as np
 
 
 def sigmoid(soma):
-    return float(1.0 / (1.0 + np.exp(-soma)))
+    return 1.0 / (1.0 + np.exp(-soma))
 
 
 entradas = np.array([
@@ -25,10 +25,32 @@ pesos0 = np.array([
     [0.358, -0.577, -0.469]
 ])
 
+pesos1 = np.array([
+    [-0.017],
+    [-0.893],
+    [0.148]
+])
+
 epocas = 100
 
 for j in range(epocas):
-    print(j)
+    camadaEntrada = entradas
+
+    somaSinapse0 = np.dot(camadaEntrada, pesos0)
+    camadaOculta = sigmoid(somaSinapse0)
+
+    somaSinapse1 = np.dot(camadaOculta, pesos1)
+    camadaSaida = sigmoid(somaSinapse1)
+
+    erroCamadaSaida = saidas - camadaSaida
+    mediaAbsoluta = np.mean(np.abs(erroCamadaSaida))
+
+    # print(mediaAbsoluta)
+
+
+    # print(j)
+
+# print("Fim")
 
 
 
